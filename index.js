37438b6565119f8f82814e3a7a6558f63ada4486
maurice diong <mdiong00@gmail.com>
2025-06-24 17:22:09 -0400

    object lab completed

--- index.js
+++ index.js
@@ -1,27 +1,53 @@
 // Write your solution in this file!
+// const employee = {};
+// employee ["name"] = "mike";
+// employee ["streetAddress"] = "the hood";
+
+// function updateEmployeeWithKeyAndValue(object, key, value) {
+//     return {
+//         ...object,
+//         [key] : value,
+//     }
+// }
+
+// function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value) {
+//     object[key] = value;
+//     return object
+// }
+
+// function deleteFromEmployeeByKey( object, key) {
+//     const newObject = {...object};
+//     delete newObject[key];
+//     return newObject;
+// }
+
+// function destructivelyDeleteFromEmployeeByKey(object, key) {
+//     delete object [ key];
+//     return object;
+// }
+
 const employee = {};
 employee ["name"] = "mike";
 employee ["streetAddress"] = "the hood";
 
 function updateEmployeeWithKeyAndValue(object, key, value) {
-    return {
-        ...object,
-        [key] : value,
-    }
+   return {...object,[key]:value}
 }
 
 function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value) {
-    object[key] = value;
+    object[key]=value
     return object
 }
 
 function deleteFromEmployeeByKey( object, key) {
-    const newObject = {...object};
-    delete newObject[key];
-    return newObject;
+    const newObject = {...object}
+    delete newObject[key]
+    return newObject
+  
 }
 
 function destructivelyDeleteFromEmployeeByKey(object, key) {
-    delete object [ key];
-    return object;
+    delete object[key]
+    return object
+   
 }
